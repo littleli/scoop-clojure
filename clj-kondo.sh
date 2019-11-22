7e3494b3530b5050613f293c9c42a8220b407640
Ales Najmann <ales.najmann@gmail.com>
2019-11-22 02:31:52 +0100

    Add native clj-kondo.exe (#8)

---
+++ clj-kondo.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+VERSION=$1
+
+# check presence of version
+if [ -z $VERSION ]; then
+	echo "Missing argument with version" >&2
+	exit 1
+fi 
+
+# check presence of tooling
+SHASUM=`which shasum`
+CURL=`which curl`
+[ -n ${SHASUM} ] && [ -n ${CURL} ] || exit 2
+
+TARGET="https://ci.appveyor.com/api/buildjobs/oqwh72id7x3q8es2/artifacts/clj-kondo-${VERSION}-windows-amd64.zip"
+
+CHECKVER_CODE=`curl -r 0-499 -sSfw "%{response_code}" --fail-early "${TARGET}"`
+if [ $CHECKVER_CODE -ne 302 ]; then
+	echo "Version ${VERSION} does not exist" >&2
+	exit 3
+fi
+
+SHA256SUM=$(curl -sLS --fail-early "${TARGET}" | shasum -a 256 -b | cut -f1 -d\ )
+
+cat <<MANIFEST  
+{
+  "description": "A linter for Clojure code that sparks joy",
+  "homepage": "https://github.com/borkdude/clj-kondo",
+  "license": "EPL-1.0",
+  "version": "${VERSION}",
+  "architecture": {
+    "64bit": {
+      "url": "https://ci.appveyor.com/api/buildjobs/oqwh72id7x3q8es2/artifacts/clj-kondo-${VERSION}-windows-amd64.zip",
+      "hash": "${SHA256SUM}"
+    }
+  },
+  "bin": "clj-kondo.exe"
+}
+
+MANIFEST
