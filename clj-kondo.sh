fb99752612f06b1de165ec86012b523351863071
Ales Najmann <ales.najmann@omio.com>
2019-11-23 19:05:30 +0100

    Update clj-kondo script

--- clj-kondo.sh
+++ clj-kondo.sh
@@ -12,9 +12,9 @@ SHASUM=`which shasum`
 CURL=`which curl`
 [ -n ${SHASUM} ] && [ -n ${CURL} ] || exit 2
 
-TARGET="https://ci.appveyor.com/api/buildjobs/oqwh72id7x3q8es2/artifacts/clj-kondo-${VERSION}-windows-amd64.zip"
+TARGET="https://github.com/borkdude/clj-kondo/releases/download/v${VERSION}/clj-kondo-${VERSION}-windows-amd64.zip"
 
-CHECKVER_CODE=`curl -r 0-499 -sSfw "%{response_code}" --fail-early "${TARGET}"`
+CHECKVER_CODE=`curl -X HEAD -m 3 -sfw "%{response_code}" ${TARGET}`
 if [ $CHECKVER_CODE -ne 302 ]; then
 	echo "Version ${VERSION} does not exist" >&2
 	exit 3
@@ -24,17 +24,21 @@ SHA256SUM=$(curl -sLS --fail-early "${TARGET}" | shasum -a 256 -b | cut -f1 -d\ 
 
 cat <<MANIFEST  
 {
-  "description": "A linter for Clojure code that sparks joy",
-  "homepage": "https://github.com/borkdude/clj-kondo",
-  "license": "EPL-1.0",
-  "version": "${VERSION}",
-  "architecture": {
-    "64bit": {
-      "url": "https://ci.appveyor.com/api/buildjobs/oqwh72id7x3q8es2/artifacts/clj-kondo-${VERSION}-windows-amd64.zip",
-      "hash": "${SHA256SUM}"
-    }
-  },
-  "bin": "clj-kondo.exe"
+    "description": "A linter for Clojure code that sparks joy",
+    "homepage": "https://github.com/borkdude/clj-kondo",
+    "license": "EPL-1.0",
+    "version": "${VERSION}",
+    "architecture": {
+        "64bit": {
+            "url": "${TARGET}",
+            "hash": "${SHA256SUM}"
+        }
+    },
+    "checkver": "github",
+    "autoupdate": {
+        "url": "https://github.com/borkdude/clj-kondo/releases/download/v\$version/clj-kondo-\$version-win-amd64.zip"
+    },
+    "bin": "clj-kondo.exe"
 }
 
 MANIFEST
