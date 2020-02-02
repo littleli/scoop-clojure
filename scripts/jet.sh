f812ed47ce2d55f8ba503d213f9abb1688ea4f64
Ales Najmann <ales.najmann@omio.com>
2020-02-02 13:12:21 +0100

    Add support for jet cli tool, initial version 0.0.11

---
+++ scripts/jet.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+VERSION=$1
+
+# check presence of version
+if [ -z $VERSION ]; then
+	echo "Missing argument with version" >&2
+	exit 1
+fi 
+
+# check presence of tooling
+SHASUM=`which shasum`
+CURL=`which curl`
+[ -n ${SHASUM} ] && [ -n ${CURL} ] || exit 2
+
+TARGET="https://github.com/borkdude/jet/releases/download/v${VERSION}/jet-${VERSION}-windows-amd64.zip"
+
+CHECKVER_CODE=`curl -X HEAD -m 3 -sfw "%{response_code}" ${TARGET}`
+if [ $CHECKVER_CODE -ne 302 ]; then
+	echo "Version ${VERSION} does not exist" >&2
+	exit 3
+fi
+
+SHA256SUM=$(curl -sLS --fail-early "${TARGET}" | shasum -a 256 -b | cut -f1 -d\ )
+
+cat <<MANIFEST  
+{
+    "description": "CLI to transform between JSON, EDN and Transit, powered with a minimal query language",
+    "homepage": "https://github.com/borkdude/jet",
+    "license": "EPL-1.0",
+    "version": "${VERSION}",
+    "architecture": {
+        "64bit": {
+            "url": "${TARGET}",
+            "hash": "${SHA256SUM}"
+        }
+    },
+    "suggest": {
+        "Visual C/C++ Redistributable 2010": "extras/vcredist2010"
+    },
+    "bin": "jet.exe"
+}
+
+MANIFEST
